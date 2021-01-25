a1c71b51be84584eb7cdfc968158bf93491ce418
Jeppehimer <77025251+Jeppehimer@users.noreply.github.com>
2021-01-25 17:04:50 -0600

    Add random quote generator

---
+++ quotesFCC.js
@@ -0,0 +1,22 @@
+const QUOTEBANK = [
+                {
+                    quote:  "The Best Way To Get Started Is To Quit Talking And Begin Doing.",
+                    author: "Walt Disney"
+                },
+                {
+                    quote:  "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.",
+                    author: "Winston Churchill"
+                },
+                {
+                    quote:  "Don’t Let Yesterday Take Up Too Much Of Today.",
+                    author: "Will Rogers"
+                },
+                {
+                    quote:  "You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character",
+                    author: "Unknown"
+                },
+                {
+                    quote:  "It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.",
+                    author: "Vince Lombardi"
+                }
+            ]
